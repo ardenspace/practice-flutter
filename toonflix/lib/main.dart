73851e3e631d152408e12fcb315d02078c389d36
arden LEE <ardensdevspace@gmail.com>
2025-08-01 06:05:15 +0900

    TIL: what about life cycle in widget tree

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -17,7 +17,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
   List<int> numbers = [];
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
 
   // 완전 리액트랑 비슷하다.
   // state의 개념이 있음 state 없이 그냥 함수를 때려 넣으면 아무것도 안 바뀜
@@ -31,12 +38,18 @@ class _AppState extends State<App> {
         // 설정 한 번으로 모든 테마를 유지할 수 있다.
         textTheme: const TextTheme(titleLarge: TextStyle(color: Colors.red)),
       ),
-      home: const Scaffold(
-        backgroundColor: Color(0xFFF4FDDB),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4FDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [MyLargeTitle()],
+            children: [
+              showTitle ? const MyLargeTitle() : const Text("nothing"),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye_rounded),
+              ),
+            ],
             // 자 그럼 이 칠드런이 저 제일 대빵 요소에 닿아야만 테마가 작동된다.
             // 이걸 도와주는 게 context임!
           ),
@@ -46,11 +59,34 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({super.key});
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  @override
+  void initState() {
+    // 상태를 초기화하기 위한 메서드
+    // 자주 쓰이진 않음
+    super.initState();
+    print("iniState!"); // build가 시작되기 전에 동작함
+  }
+
+  @override
+  void dispose() {
+    // 위젯트리에서 위젯이 제거될 때에 dispose 메서드가 발생함
+    super.dispose();
+    print("dispose!");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("build!");
     return Text(
       "My Large Title",
       style: TextStyle(
